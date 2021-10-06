0ed2f960c807263f54e54d3cd212b544534b6248
choa03 <1110aaron@naver.com>
2021-10-06 20:43:34 +0900

    Update 06. get_full_screen_tile.py

--- lab_mario/06. get_full_screen_tile.py
+++ lab_mario/06. get_full_screen_tile.py
@@ -13,7 +13,7 @@ full_screen_tiles = ram[0x0500:0x069F+1]
 print(full_screen_tiles.shape)
 print(full_screen_tiles)
 
-full_screen_tile_count = full_screen_tiles.shape[0] 
+full_screen_tile_count = full_screen_tiles.shape[0]
 
 full_screen_page1_tile = full_screen_tiles[:full_screen_tile_count//2].reshape((13, 16))
 full_screen_page2_tile = full_screen_tiles[full_screen_tile_count//2:].reshape((13, 16))
