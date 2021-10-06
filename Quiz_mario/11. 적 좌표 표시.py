a3b735ec76b1bd8e0672db775ac76359b903dac5
choa03 <1110aaron@naver.com>
2021-10-06 20:58:51 +0900

    Update 11. 적 좌표 표시.py

--- Quiz_mario/11. 적 좌표 표시.py
+++ Quiz_mario/11. 적 좌표 표시.py
@@ -133,7 +133,7 @@ class MyApp(QWidget):
         enemy_position_y = ram[0x00CF:0x00d3 + 1]
         # 적 x 좌표
         enemy_position_x = (enemy_horizon_position * 256 + enemy_screen_position_x) % 512
- 
+
         # enemy_position_x = ram[0x0087:0x008B + 1]
         # enemy_position_y = ram[0x00CF:0x00d3 + 1]
         # 적 타일 좌표
